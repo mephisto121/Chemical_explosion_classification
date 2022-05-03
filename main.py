ee7b69faec3f47a662c1111234b2be866889d29c
mephisto121 <71381384+mephisto121@users.noreply.github.com>
2022-05-03 09:16:39 +0430

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def parse_args():
 
     parser = argparse.ArgumentParser(description='Run Chemical explosion prediction from command line')
     parser.add_argument('-s', '--smiles', default=None, type = str, 
-                        help='SMILES input for yield predictions')
+                        help='SMILES input for Chemical explosion prediction')
     parser.add_argument('-n', '--name', default='test_mol', help='The name of the molecule')
 
     return parser.parse_args()
